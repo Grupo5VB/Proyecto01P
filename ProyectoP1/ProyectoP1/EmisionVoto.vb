23d92a47294eb03c264d679e44f2a72a9299facc
Olguis <Olguis@OLGUIS-PC>
2016-11-30 22:51:05 -0500

    Creacion de clases ->(Estadistica y Emision de votos)

---
+++ ProyectoP1/ProyectoP1/EmisionVoto.vb
@@ -0,0 +1,11 @@
+﻿Public Class EmisionVoto
+    Private _numCertficado As String
+    Public Property numCertificado() As String
+        Get
+            Return _numCertficado
+        End Get
+        Set(ByVal value As String)
+            _numCertficado = value
+        End Set
+    End Property
+End Class
---
+++ ProyectoP1/ProyectoP1/Estadistica.vb
@@ -0,0 +1,41 @@
+﻿Public Class Estadistica
+    Private _totalVotos As Integer
+    Public Property totalVotos() As Integer
+        Get
+            Return _totalVotos
+        End Get
+        Set(ByVal value As Integer)
+            _totalVotos = value
+        End Set
+    End Property
+
+    Private _votosValidos As Integer
+    Public Property votosValidos() As Integer
+        Get
+            Return _votosValidos
+        End Get
+        Set(ByVal value As Integer)
+            _votosValidos = value
+        End Set
+    End Property
+
+    Private _votosNulos As Integer
+    Public Property votosNulos() As Integer
+        Get
+            Return _votosNulos
+        End Get
+        Set(ByVal value As Integer)
+            _votosNulos = value
+        End Set
+    End Property
+
+    Private _votosBlancos As Integer
+    Public Property votosBlancos() As Integer
+        Get
+            Return _votosBlancos
+        End Get
+        Set(ByVal value As Integer)
+            _votosBlancos = value
+        End Set
+    End Property
+End Class

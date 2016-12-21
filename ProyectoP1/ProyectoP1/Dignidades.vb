d13ba02c75ee6fe23c5347f13867528da7bd95ac
Olguis <Olguis@OLGUIS-PC>
2016-12-21 08:45:46 -0500

    creacion clase Dignidad

---
+++ ProyectoP1/ProyectoP1/Dignidades.vb
@@ -0,0 +1,21 @@
+﻿Public Class Dignidades
+    Private _codigoDignidad As String
+    Public Property codigoDignidad() As String
+        Get
+            Return _codigoDignidad
+        End Get
+        Set(ByVal value As String)
+            _codigoDignidad = value
+        End Set
+    End Property
+
+    Private _nombreDignidad As String
+    Public Property nombreDignidad() As String
+        Get
+            Return _nombreDignidad
+        End Get
+        Set(ByVal value As String)
+            _nombreDignidad = value
+        End Set
+    End Property
+End Class
